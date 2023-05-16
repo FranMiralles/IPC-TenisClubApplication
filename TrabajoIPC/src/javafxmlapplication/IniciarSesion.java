f86bf6c214eab88e9d352af4ceecdbf49407b403
Guillem <guillemfus75@gmail.com>
2023-05-16 19:02:49 +0200

    Iniciar Sesión
    Holi, he añadido algo de código al iniciar sesión, ahora comprueba en tiempo de escritura si hay algún error (No he conseguido que compruebe si el usuario no está registrado, no me detecta el existLogin :)).

--- TrabajoIPC/src/javafxmlapplication/IniciarSesion.java
+++ TrabajoIPC/src/javafxmlapplication/IniciarSesion.java
@@ -40,7 +40,39 @@ public class IniciarSesion implements Initializable {
         
         alertImage.setVisible(false);
         
+        //Permitir todos los caracteres excepto el espacio en el nickname
+        user.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.contains(" ")) {
+                user.setText(oldValue);
+            }
+        });
         
+        //Errores en nickname no usado
+        user.setOnKeyTyped(event -> cambiarEstiloNick());
+        
+        //Errores en nickname vacío
+        user.setOnKeyTyped(event -> cambiarEstiloNickEmpty());
+        
+        //Errores en nickname cuando se cambie de textField
+        user.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                cambiarEstiloNickEmpty();
+            }
+        });
+        
+        //Permitir solo números y letras en el campo de contraseña
+        pwd.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("[a-zA-Z0-9]*")) {
+                pwd.setText(oldValue);
+            }
+        });
+        
+        //Errores en pwd cuando se cambie de textField
+        pwd.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                cambiarEstiloPwdEmpty();
+            }
+        });
     }    
     
     @FXML
@@ -71,7 +103,8 @@ public class IniciarSesion implements Initializable {
         }else{
             alertImage.setVisible(true);
             alertText.setStyle("-fx-text-fill: #fc0000;");
-            alertText.setText("Campo vacío");
+            if(user.getText().isEmpty()){alertText.setText("Campo usuario vacío");}
+            else{alertText.setText("Campo contraseña vacío");}
             if(user.getText().isBlank()){ user.requestFocus();}
             else{ pwd.requestFocus();}
         }
@@ -96,5 +129,51 @@ public class IniciarSesion implements Initializable {
         alertImage.setVisible(false);
         alertText.setText("Inicia con tus credenciales");
         alertText.setStyle("-fx-text-fill: #7c7c7c;");
+    }
+    //Mostrar errores en nickname cuando no está registrado
+    private void cambiarEstiloNick() {
+        boolean nickValido = greenBall.existsLogin(user.getText());
+
+        if (!nickValido) {
+            alertImage.setVisible(true);
+            alertText.setStyle("-fx-text-fill: #fc0000;");
+            alertText.setText("No existe usuario con ese nombre");
+        } else {
+            alertImage.setVisible(false);
+            alertText.setStyle("-fx-text-fill: #7c7c7c;");
+            alertText.setText("Inicia con tus credenciales");
+        }
+    }
+    
+    //Mostrar errores en nickname cuando está vacío
+    private void cambiarEstiloNickEmpty() {
+        String nickText = user.getText();
+        boolean nickValido = !nickText.isEmpty();
+
+        if (!nickValido) {
+            alertImage.setVisible(true);
+            alertText.setStyle("-fx-text-fill: #fc0000;");
+            alertText.setText("Campo usuario vacío");
+        } else {
+            alertImage.setVisible(false);
+            alertText.setStyle("-fx-text-fill: #7c7c7c;");
+            alertText.setText("Inicia con tus credenciales");
+        }
+    }
+    
+    //Mostrar errores en pwd cunado está vaío
+    private void cambiarEstiloPwdEmpty() {
+        String pwdText = pwd.getText();
+        boolean pwdValido = !pwdText.isEmpty();
+
+        if (!pwdValido) {
+            alertImage.setVisible(true);
+            alertText.setStyle("-fx-text-fill: #fc0000;");
+            alertText.setText("Campo contraseña vacío");
+        } else {
+            alertImage.setVisible(false);
+            alertText.setStyle("-fx-text-fill: #7c7c7c;");
+            alertText.setText("Inicia con tus credenciales");
+        }
     }
 }
